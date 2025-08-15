d8d4790862892df3b73fe8244bd223a6a24f239b
TiffyAI <quentenmeyer@gmail.com>
2025-08-15 15:12:26 +0200

    Update service-worker.js

--- App/service-worker.js
+++ App/service-worker.js
@@ -2,8 +2,8 @@ self.addEventListener('install', e => {
     e.waitUntil(
         caches.open('motion-power-sphere-v1').then(cache => {
             return cache.addAll([
-                '/Motion-Power-Sphere/app/',
-                '/Motion-Power-Sphere/app/index.html'
+                '/Install/app/',
+                '/Install/app/index.html'
             ]);
         })
     );
